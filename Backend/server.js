5e40355e207dd3e5e12e25f56d219ed52d6240cd
Xaad989 <l215344@lhr.nu.edu.pk>
2025-03-07 17:15:06 +0500

    Connected to Atlas

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,12 @@
-import express from "express";
-import mongoose from "mongoose";
-import connect from "./utils/dbConnect";
-
-
-
-
+import express from 'express';
+import mongoose from 'mongoose';
+import connect from './utils/dbConnect.js';
 
 await connect();
 
-const app=express();
-const port=8080;
+const app = express();
+const port = 8080;
 
-
-
-app.listen(port,()=>{
-    console.log("SERVER IS ON!!");
-})
+app.listen(port, () => {
+	console.log('SERVER IS ON!!');
+});
--- Backend/utils/dbConnect.js
+++ Backend/utils/dbConnect.js
@@ -1,14 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-const connect=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("DB CONNECTED !")
-    } catch (error) {
-        console.log("Error Connecting ",error)
-    }
-}
-
+const connect = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log('DB CONNECTED !');
+	} catch (error) {
+		console.log('Error Connecting ', error);
+	}
+};
 
 export default connect;
