b4f67d2adf3bdce68d7ff0c25760c5aa8fb501ac
Asmar Shahid <l211754@lhr.nu.edu.pk>
2025-03-17 17:10:55 +0500

    Inbox page

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -34,8 +34,8 @@ const Inbox = () => {
 		setInputMessage('');
 	};
 
-	const bgClr = 'bg-[#111820]';
-	const fgClr = 'bg-[#282e3b]';
+	const bgClr = 'bg-[#f2f3f8]';
+	const fgClr = 'bg-[#3f51b5]';
 	const borderClr = 'border-[#282e3b]';
 	const borderBgClr = 'border-[#111820]';
 
@@ -45,13 +45,13 @@ const Inbox = () => {
 			<LeftSideBar></LeftSideBar>
 
 			{/* Chat List */}
-			<div className={`w-1/6 ${bgClr} border-r ${borderClr} p-[0.8vw]`}>
-				<div className='relative mb-[1vw]'>
-					<Search className='absolute left-[0.7vw] top-[0.7vw] text-[#fbfbfb]' />
+			<div className={`w-1/6 bg-[#3f51b5] p-[0.8vw] absolute h-full left-1/7`}>
+				<div className='relative mb-[1vw] shadow-lg'>
+					<Search className='absolute left-[0.7vw] top-[0.7vw] text-[#333333]' />
 					<input
 						type='text'
 						placeholder='Search...'
-						className={`w-full rounded-sm pl-[2.5vw] pr-[0.8vw] py-[0.5vw] ${fgClr} text-white focus:outline-none text-[0.9vw]`}
+						className={`w-full rounded-sm pl-[2.5vw] pr-[0.8vw] py-[0.5vw] bg-[#ffffff] text-[#333333] focus:outline-none text-[0.9vw]`}
 						value={searchQuery}
 						onChange={(e) => setSearchQuery(e.target.value)}
 					/>
@@ -60,11 +60,9 @@ const Inbox = () => {
 					{filteredChats.map((chat) => (
 						<div
 							key={chat}
-							className={`flex items-center gap-[0.5vw] p-[0.5vw] select-none px-[1vw] hover:${fgClr} transition py-[0.8vw] rounded-sm cursor-pointer font-bold ${
-								selectedChat === chat ? `${fgClr} text-white` : 'bg-darkgray'
-							}`}
-							onClick={() => setSelectedChat(chat)}
-						>
+							className={`flex items-center gap-[0.5vw] p-[0.5vw] select-none px-[1vw] hover:bg-[#4e5fbb] transition py-[0.8vw] rounded-sm cursor-pointer font-bold ${
+								selectedChat === chat && `bg-[#4e5fbb]`}`}
+							onClick={() => setSelectedChat(chat)}>
 							<img
 								src={profileImage}
 								className='w-[2vw] h-[2vw] rounded-full'
@@ -77,18 +75,19 @@ const Inbox = () => {
 			</div>
 
 			{/* Chat Window */}
-			<div className={`flex-1 flex flex-col ${bgClr}`}>
+			<div className={`flex-1 flex flex-col ${bgClr} border-l border-[#4e5fbb]`}>
 				{selectedChat ? (
 					<>
 						<div
-							className={`px-[1vw] py-[0.7vw] border-b ${borderClr} cursor-pointer text-lg font-bold flex items-center gap-[0.7vw]`}
-						>
+							className={`px-[1vw] py-[0.8vw] bg-[#3f51b5] cursor-pointer text-lg font-bold flex items-center gap-[0.7vw]`}>
 							<img
 								src={profileImage}
-								className='w-[2vw] h-[2vw] rounded-full'
-								alt='Profile'
-							/>
-							<p>{selectedChat}</p>
+								className='w-[2.2vw] h-[2.2vw] rounded-full'
+								alt='Profile' />
+							<div>
+								<p className='m-0 p-0 text-[0.8vw]'>{selectedChat}</p>
+								<p className='m-0 p-0 text-[0.6vw] opacity-50 font-normal'>online</p>
+							</div>
 						</div>
 						<div className='flex-1 p-[1vw] overflow-y-auto'>
 							{(messages[selectedChat] || []).map((msg, index) => (
@@ -96,13 +95,12 @@ const Inbox = () => {
 									key={index}
 									className={`mb-[0.2vw] ${
 										msg.sender === 'You' ? 'text-right' : 'text-left'
-									}`}
-								>
+									}`}>
 									<span
-										className={`inline-block pl-[0.5vw] pr-[2vw] py-[0.3vw] rounded-sm text-[0.7vw] ${
+										className={`inline-block pl-[0.5vw] pr-[2vw] py-[0.5vw] rounded-sm text-[0.7vw] ${
 											msg.sender === 'You'
-												? 'bg-blue-400 text-white'
-												: `${fgClr}`
+												? 'bg-[#3f51b5]'
+												: `bg-[#4e5fbb]`
 										}`}
 									>
 										{msg.text}
@@ -111,11 +109,10 @@ const Inbox = () => {
 							))}
 						</div>
 						<div
-							className={`p-[0.5vw] border-t ${borderClr} flex items-center`}
-						>
+							className={`p-[0.5vw] bg-[#3f51b5] flex items-center`}>
 							<input
 								type='text'
-								className={`flex-1 ${fgClr} rounded-sm px-[1vw] py-[0.8vw] text-[0.8vw] focus:outline-none`}
+								className={`flex-1 bg-[#4e5fbb] rounded-sm px-[1vw] py-[0.8vw] text-[0.8vw] focus:outline-none`}
 								value={inputMessage}
 								onChange={(e) => setInputMessage(e.target.value)}
 								placeholder='Type a message...'
@@ -127,14 +124,13 @@ const Inbox = () => {
 							/>
 							<div
 								onClick={handleSendMessage}
-								className={`ml-2 ${fgClr} px-[1vw] py-[0.9vw] h-auto rounded-sm cursor-pointer text-[1vw] text-gray-500 hover:text-blue-400 transition`}
-							>
+								className={`ml-2 bg-[#4e5fbb] px-[1vw] py-[0.9vw] h-auto rounded-sm cursor-pointer text-[1vw] text-[#cccccc] hover:text-[#ffffff] duration-300 transition`}>
 								<Send className={``} />
 							</div>
 						</div>
 					</>
 				) : (
-					<div className='flex items-center justify-center flex-1 text-gray-500'>
+					<div className='flex items-center justify-center flex-1 text-gray-500 opacity-50'>
 						Select a chat to start messaging
 					</div>
 				)}
